ce4a0ee07dc9d2f31b452dd5fae8d49e229f1e78
Dayana-K-H <harahapdayana01@gmail.com>
2025-06-23 13:25:34 +0700

    Secure Supabase API keys using environment variables and update app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecotrack_app/views/carbon_log_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
 import 'firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -9,19 +10,22 @@ import 'view_models/auth_view_model.dart';
 import 'view_models/carbon_log_view_model.dart';
 import 'views/splash_view.dart';
 import 'views/auth_view.dart';
-import 'views/home_dashboard_view.dart'; 
-import 'views/log_transport_activity_view.dart'; 
-import 'views/log_electricity_activity_view.dart'; 
-import 'views/log_waste_activity_view.dart'; 
+import 'views/home_dashboard_view.dart';
+import 'views/log_transport_activity_view.dart';
+import 'views/log_electricity_activity_view.dart';
+import 'views/log_waste_activity_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Supabase.initialize(
-    url: 'https://fummnjzxjkkojcoapmpp.supabase.co', 
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1bW1uanp4amtrb2pjb2FwbXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MDA1MDksImV4cCI6MjA2NjE3NjUwOX0.O_7XOSWTMSRljOsWF-XcB8QaNG62hAN9QnesdNCeU4s',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(
@@ -52,13 +56,13 @@ class _MyAppState extends State<MyApp> {
       ),
       home: SplashView(),
       routes: {
-        '/splash': (context) =>  SplashView(),
+        '/splash': (context) => SplashView(),
         '/auth_view': (context) => AuthView(),
         '/home_dashboard': (context) => const HomeDashboard(),
-        '/carbon_log': (context) => const CarbonLogView(), 
-        '/log_transport': (context) => const LogTransportActivityView(), 
+        '/carbon_log': (context) => const CarbonLogView(),
+        '/log_transport': (context) => const LogTransportActivityView(),
         '/log_electricity': (context) => const LogElectricityActivityView(),
-        '/log_waste': (context) => const LogWasteActivityView(), 
+        '/log_waste': (context) => const LogWasteActivityView(),
       },
     );
   }
